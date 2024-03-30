023157e769a3912b2ef52cb23046a2ba2955e88c
tudo75 <tudo75@hotmail.com>
2024-03-30 18:24:13 +0100

    Added Italian translation

---
+++ src/Language/it/Bonfire.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file is part of Bonfire.
+ *
+ * (c) Lonnie Ezell <lonnieje@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+return [
+    'resourceNotFound'     => '{0} non trovato.',
+    'resourceSaved'        => '{0} salvato.',
+    'resourcesSaved'       => '{0} salvati.',
+    'resourceDeleted'      => '{0} eliminato.',
+    'resourcesDeleted'     => '{0} eliminati.',
+    'resourceRestored'     => '{0} ripristinato.',
+    'unknownSaveError'     => 'Errore sconosciuto durante il slavataggio di {0}.',
+    'unknownError'         => 'Errore sconosciuto.',
+    'resourcesNotSelected' => 'Nessun {0} selezionato.',
+
+    'deleteResource' => 'Eliminare questo {0}?',
+    'delete'         => 'Elimina',
+    'edit'           => 'Modifica',
+
+    'notAuthorized' => 'Non hai i permessi per accedere.',
+    'unknownAction' => 'L\'azione da te richiesta non è stata riconosciuta.',
+    'magicLinkInfo' => 'Ti sarà inviata un email con un link per effettuare l\'accesso.',
+
+    'lastModified' => 'Ultima modifica',
+    'fileSize'     => 'Dimensione del file',
+
+    //collections:
+    'contentModTitle'   => 'Contenuto',
+    'settingsModTitle'  => 'Impostazioni',
+    'toolsModTitle'     => 'Strumenti',
+    'generalModTitle'   => 'Generali',
+
+    //theme:
+    'filter'            => 'Filtro',
+    'dashboard'         => 'Dashboard',
+    'searchResults'     => 'Risultati della ricerca',
+    'nothingFound'      => 'Nessun risultato per i tuoi criteri di ricerca. Rivedi i criteri di ricerca e riprova.',
+    'seeAll'            => 'Visualizza tutto',
+    'search'            => 'Cerca',
+    'myAccount'         => 'Il mio profilo',
+    'signOut'           => 'Esci',
+    'offlineNotice'     => 'Il sito è attualmente non in linea. Attivalo.',
+    'here'              => 'qui',
+    'collapseSidebar'   => 'Minimizza la barra laterale',
+
+    // Dashboard:
+    'quickLinks'        => 'Collegamenti veloci',
+
+    //users module Search template:
+    'email'         => 'Email',
+    'username'      => 'nome utente',
+    'groups'        => 'Ruoli',
+    'lastActive'    => 'Ultima attività',
+
+];
---
+++ src/Tools/Language/it/Tools.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of Bonfire.
+ *
+ * (c) Lonnie Ezell <lonnieje@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+return [
+    'systemInfoModTitle'    => 'Informazioni di sistema',
+    'logsModTitle'          => 'Registri',
+    'log'                   => 'Registro',
+    'empty'                 => 'Nessun registro trovato.',
+    'level'                 => 'Livello',
+    'date'                  => 'Data',
+    'file'                  => 'Nome del file',
+    'content'               => 'Contenuto',
+    'deleteFile'            => 'Elimina il file di registro',
+    'deleteSelected'        => 'Elimina la selezione',
+    'deleteAll'             => 'Elimina tutti i registri',
+    'deleteConfirm'         => 'Sei sicuro di voler eliminare il file corrente?',
+    'deleteSelectedConfirm' => 'Sei sicuro di voler eliminare tutti i file selezionati?',
+    'deleteAllConfirm'      => 'Sei sicuro di voler eliminare tutti i file di registro?',
+    'deleteSuccess'         => 'File eliminati con successo.',
+    'deleteAllSuccess'      => 'Tutti i file sono stati eliminati con successo.',
+    'deleteError'           => 'Impossibile eseguire l\'eliminazione.',
+];
